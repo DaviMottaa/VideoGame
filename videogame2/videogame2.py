97d8997a6827594e3f8597de3af7d27735b4c6ff
ninaemotta@gmail.com <ninaemotta@gmail.com>
2021-10-18 15:58:04 -0300

    Adicionar foguete

--- videogame2/videogame2.py
+++ videogame2/videogame2.py
@@ -1,1 +1,15 @@
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((600,400))
 
+    
+
+
+
+
+
+image = pygame.image.load("ship.bmp") 
+while True:
+    screen.fill((230,230,230))
+    screen.blit(image,image.get_rect())
+    pygame.display.flip()
